f2902bcc475085f7898b433e64da64f51e66e568
MetelAnna <metel.anna.02@gmail.com>
2024-10-01 20:10:16 +0300

    First Commit. HW lesson 01.

---
+++ Homework/src/edu/training/jc/simple_output/FrameOutput.java
@@ -0,0 +1,14 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput {
+
+	public static void main(String[] args) {
+
+		System.out.print("* * * * * * * * *");
+		System.out.print("\n*\t*\t*");
+		System.out.print("\n*\t*\t*");
+		System.out.print("\n*\t*\t*");
+		System.out.print("\n* * * * * * * * *");
+	}
+
+}
---
+++ Homework/src/edu/training/jc/simple_output/StairsOutput.java
@@ -0,0 +1,14 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\"To wisely live your life, you don't need to know much");
+		System.out.println("\n\tJust remember two main rules for the beginning:");
+		System.out.println("\n\t\tYou better starve, than eat whatever");
+		System.out.println("\n\tAnd better be alone, than with whoever.\"");
+		System.out.println("\nOmar Khayyam, Rubaiyat");
+	}
+
+}
